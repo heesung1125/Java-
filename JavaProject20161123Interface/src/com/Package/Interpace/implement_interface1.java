b3113294a68f56a1872e6fa69e3bcb0c57ba5b57
heesung1125 <heesung.lee1125@gmail.com>
2016-11-23 19:52:19 +0900

    2016-11-23 강의노트

---
+++ JavaProject20161123Interface/src/com/Package/Interpace/implement_interface1.java
@@ -0,0 +1,20 @@
+package com.Package.Interpace;
+
+public class implement_interface1 implements Interface_test {
+
+	@Override
+	public void ifMethod1(int age, String name) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	@Override
+	public void ifMethod2(int salary, String subject) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
